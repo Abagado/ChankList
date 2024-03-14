d23fd611a51b05ee6b7ab3617b967b3adeae25be
Abagado <147521764+Abagado@users.noreply.github.com>
2024-03-14 20:09:13 +1000

    Update chunklist.cpp

--- chunklist.cpp
+++ chunklist.cpp
@@ -1,4 +1,4 @@
-﻿#include "chunk.h"
+#include "chunk.h"
 #include <iostream>
 
 using namespace std;
@@ -7,12 +7,9 @@ int main()
 {
     fefu_laboratory_two::ChunkList<int, 3> chunkList;
 
-    // Вставка элементов
     for (int i = 1; i <= 10; ++i)
         chunkList.insert(i);
 
-
-    // Вывод содержимого после вставки
     cout << "ChunkList after insertions:" << endl;
     for (const auto& chunk : chunkList)
     {
@@ -22,12 +19,11 @@ int main()
         cout << endl;
     }
 
-    // Удаление элементов
-    chunkList.erase(2);
+    chunkList.erase(1);
+    chunkList.erase(4);
     chunkList.erase(5);
-    chunkList.erase(8);
+    chunkList.erase(6);
 
-    // Вывод содержимого после удалений
     cout << "ChunkList after erasures:" << endl;
     for (const auto& chunk : chunkList)
     {
